a9e077b0ca4b127cbfd6b310fefb924a1c0d2cac
caijun <987985143@qq.com>
2019-08-29 10:39:59 +0800

    create database stu

---
+++ database/migrations/2019_08_29_023139_create_table_stu.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableStu extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //创建表 stu是你的表名
+        Schema::create('stu', function (Blueprint $table) {
+        //increments string timestamps 类型
+        $table->increments('id');
+        $table->string('name');
+        $table->string('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //删除表
+        Schema::drop('stu');
+    }
+}
